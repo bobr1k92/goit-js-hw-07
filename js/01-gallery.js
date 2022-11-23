6815eaeb9a4deefb5b5d00c4fa2a536cee7d7040
Denis Mazurov <19bobrik92@gmail.com>
2022-11-23 05:12:45 +0200

    task-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemMarkup = galleryItems.map(item => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`
+}).join('');
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemMarkup);
+
+galleryContainer.addEventListener('click', onClick);
+
+function onClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+      return;
+   }
+    
+    const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}"/>`, {
+        onShow: (instance) => {
+            document.addEventListener('keydown', onEscape);
+        },
+
+        onClose: (instance) => {
+            document.removeEventListener('keydown', onEscape);
+        },
+    }
+    );
+    instance.show();
+
+    function onEscape(evt) {
+        if (evt.key === 'Escape') {
+            instance.close();
+        }
+   } 
+}
+
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryItems
+   .map(item => {
+      return `<a class="gallery__item" href="${item.original}">
+   <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+   })
+   .join('');
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+const lightbox = new SimpleLightbox('.gallery__item', {
+   captionsData: 'alt',
+   captionDelay: 250,
+});
